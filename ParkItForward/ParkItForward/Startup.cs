68ef1e7efe181f3802351a8bf652c71516695974
Pramod K <pramod.kotipalli@gmail.com>
2015-08-29 10:38:42 -0400

    Initial commit

---
+++ ParkItForward/ParkItForward/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ParkItForward.Startup))]
+namespace ParkItForward
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
