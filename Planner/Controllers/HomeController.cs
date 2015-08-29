f4cf270b04f3b3202b258e1843a4f1d6b3c0f68b
Pramod K <pramod.kotipalli@gmail.com>
2015-08-29 16:58:58 -0400

    Remove home

--- Planner/Controllers/HomeController.cs
+++ Planner/Controllers/HomeController.cs
@@ -5,21 +5,5 @@ namespace Planner.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            if (Session["EmailAddress"] != null)
-            {
-                ViewBag.IsLoggedIn = true;
-                ViewBag.EmailAddress = Session["EmailAddress"] as string;
-                return View();
-                //return RedirectToAction("Secured");
-            }
-            if (Session["LoginFailed"] != null)
-            {
-                ViewBag.LoginFailed = true;
-            }
-            ViewBag.IsLoggedIn = false;
-            return View();
-        }
     }
 }
